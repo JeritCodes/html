70d646ddb6f1ba394f08afdad4776f731acb979d
JeritCodes <jeritreji21@gmail.com>
2024-08-09 15:03:25 +0530

    1st commmit

---
+++ script.js
@@ -0,0 +1,6 @@
+function fn(){
+    var a=document.getElementById('ta').value;
+document.getElementById('par').innerHTML=a;
+
+window.location.assign("https://mail.google.com/mail/u/0/#inbox?compose=new");
+}
